d2d4aa30961c8af45ce15b14598145f70f9b8cfe
kholliday88 <kholliday88@gmail.com>
2016-03-19 17:34:41 -0400

    DONE - just need to go back and change CSS/SCSS (Thanks again, Brackets)

--- chatapp.js
+++ chatapp.js
@@ -25,6 +25,14 @@ window.addEventListener('load', function () {
                 var data = JSON.parse(request.responseText);
                 for (var i = 0; i < data.length; i++) {
                     console.log(data[i].user, data[i].message);
+                    var serverUser = document.createElement('h4');
+                    var serverMessage = document.createElement('p');
+                    var display = document.getElementById("display-messages");
+                    serverUser.textContent = data[i].user;
+                    serverMessage.textContent = data[i].message;
+                    console.log(serverUser, serverMessage);
+                    display.appendChild(serverUser);
+                    display.appendChild(serverMessage);
                 }
             };
             request.send();
